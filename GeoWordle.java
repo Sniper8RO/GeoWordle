3483ffbbee4cab42f23b96f19105fdf59596e60b
Sniper8RO <dennis_sniper@yahoo.com>
2023-10-09 12:54:32 +0200

    Add files via upload

---
+++ GeoWordle.java
@@ -0,0 +1,34 @@
+/**
+ * Simple wordle game variation entitled GeoWordle.
+ * @name Dennis-Cristian Baractaru
+ * @studentID <1956957>
+ * @name
+ * @studentID
+ * @date 07/10/2023
+ */
+
+public class GeoWordle {
+    //Displaying the interface (boxes + intro message)
+
+    //Secret word finder: Category selection + Randompicker of wordle
+    public String solution() {
+        String secretWord;
+        
+        return secretWord;
+    }
+
+    //Analysis of input: displaying the colours
+    public boolean correctLetter() {
+
+        return false;
+    }
+
+    public boolean correctPosition() {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
